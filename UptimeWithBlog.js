56e5df244eb0fec4801a4e4c27b7aaa69f2effd5
lifeofsoumya <mondalsoumya2001@gmail.com>
2024-10-15 22:56:06 +0530

    uptime to do tracker and uptime tracker

--- UptimeWithBlog.js
+++ UptimeWithBlog.js
@@ -1,10 +1,11 @@
 const websites = [
-    { name: "Ocode", url: "https://www.google.com", logo: "https://static-logosm.s3.ap-south-1.amazonaws.com/ocode_mini.png" },
-    { name: "Nated", url: "https://www.github.com", logo: "https://static-logosm.s3.ap-south-1.amazonaws.com/pepeso.png" },
-    { name: "Pepeso", url: "https://stackoverfl.com", logo: "https://static-logosm.s3.ap-south-1.amazonaws.com/sm.png" }
+    { name: "Ocode", url: "https://ocode.dev", logo: "https://static-logosm.s3.ap-south-1.amazonaws.com/ocode_mini.png" },
+    { name: "Nated", url: "https://pepeso.com", logo: "https://static-logosm.s3.ap-south-1.amazonaws.com/pepeso.png" },
+    { name: "Pepeso", url: "https://soumyamondal.com", logo: "https://static-logosm.s3.ap-south-1.amazonaws.com/sm.png" }
 ];
 
-let widget = await createWidget();
+let api = await todoApi()
+let widget = await createWidget(api)
 
 if (config.runsInWidget) {
     Script.setWidget(widget);
@@ -14,7 +15,7 @@ if (config.runsInWidget) {
 
 Script.complete();
 
-async function createWidget() {
+async function createWidget(api) {
     let w = new ListWidget();
     w.setPadding(15, 15, 15, 15);
 
@@ -22,26 +23,25 @@ async function createWidget() {
     let gradient = new LinearGradient();
     gradient.locations = [0, 1];
     gradient.colors = [
-        new Color("#b00a0fe6"),
-        new Color("#b00a0fb3")
+        new Color("141414"),
+        new Color("13233F")
     ];
     w.backgroundGradient = gradient;
 
     // Add logo image
     const logoImage = await loadImage("https://d1quwwdmdfumn6.cloudfront.net/t3n/2018/images/logos/t3n_logo_420x420.png");
     const logo = w.addImage(logoImage);
-    logo.imageSize = new Size(60, 60);
-    logo.centerAlignImage();
+    logo.imageSize = new Size(20, 20);
+    // logo.centerAlignImage();
     
     // Add heading
-    const heading = w.addText("Your Heading");
+    const heading = w.addText(api.name);
     heading.font = Font.boldSystemFont(18);
     heading.textColor = Color.white();
     w.addSpacer(5);
 
     // Fetch paragraph data from an API or RSS feed
-    const paragraphText = await fetchParagraphData(); // Assume this function fetches a paragraph
-    const paragraph = w.addText(paragraphText);
+    const paragraph = w.addText(api.description);
     paragraph.font = Font.systemFont(14);
     paragraph.textColor = Color.white();
     paragraph.lineLimit = 3; // Limit to 3 lines
@@ -53,49 +53,54 @@ async function createWidget() {
     
     const buttonText = buttonStack.addText("Read More");
     buttonText.font = Font.boldSystemFont(14);
-    buttonText.textColor = Color.white();
+    buttonText.textColor = Color.blue();
 
-    const arrowImage = await loadImage("https://static-logosm.s3.ap-south-1.amazonaws.com/pepeso.png"); // Replace with actual arrow icon URL
-    const arrowIcon = buttonStack.addImage(arrowImage);
-    arrowIcon.imageSize = new Size(20, 20);
+
+    let docsSymbol = SFSymbol.named("arrow.up.forward")
+    let docsElement = buttonStack.addImage(docsSymbol.image)
+    docsElement.imageSize = new Size(20, 20)
+    docsElement.tintColor = Color.blue()
+    docsElement.imageOpacity = 0.5
+    docsElement.url = api.url
+
+    // const arrowImage = await loadImage("https://static-logosm.s3.ap-south-1.amazonaws.com/pepeso.png"); // Replace with actual arrow icon URL
+    // const arrowIcon = buttonStack.addImage(arrowImage);
+    // arrowIcon.imageSize = new Size(20, 20);
     
     w.addSpacer(10);
+    
 
     // Create a horizontal stack for website status boxes
     const statusStack = w.addStack();
     statusStack.layoutHorizontally();
     
-    let statuses = await checkWebsites(websites); // Check website statuses
+    let statuses = await checkWebsites(websites);
 
     for (let i = 0; i < websites.length; i++) {
         let box = statusStack.addStack();
         box.layoutVertically();
-        box.cornerRadius = 10;
-        box.setPadding(10, 10, 10, 10);
+        box.cornerRadius = 7;
+        box.setPadding(2, 2, 2, 2);
         box.backgroundColor = statuses[i] ? new Color("#00FF00", 0.3) : new Color("#FF0000", 0.3); // Green or red
 
         const logoStatusImage = await loadImage(websites[i].logo);
         let logoStatus = box.addImage(logoStatusImage);
-        logoStatus.imageSize = new Size(30, 30);
+        logoStatus.imageSize = new Size(20, 20);
         logoStatus.centerAlignImage();
 
         box.addSpacer(5);
-        let statusText = box.addText(websites[i].name);
-        statusText.font = Font.systemFont(12);
-        statusText.textColor = Color.white();
         
         statusStack.addSpacer(8); // Space between boxes
     }
 
-    // Add rocket icon at the right bottom
-    const rocketIconUrl = "https://static-logosm.s3.ap-south-1.amazonaws.com/pepeso.png"; // Replace with actual rocket icon URL
-    const rocketIconImage = await loadImage(rocketIconUrl);
+    // const rocketIconUrl = "https://static-logosm.s3.ap-south-1.amazonaws.com/pepeso.png"; // Replace with actual rocket icon URL
+    // const rocketIconImage = await loadImage(rocketIconUrl);
     
-    const rocketIconStack = w.addStack();
-    rocketIconStack.layoutHorizontally();
+    // const rocketIconStack = w.addStack();
+    // rocketIconStack.layoutHorizontally();
     
-    const rocketIcon = rocketIconStack.addImage(rocketIconImage);
-    rocketIcon.imageSize = new Size(40, 40);
+    // const rocketIcon = rocketIconStack.addImage(rocketIconImage);
+    // rocketIcon.imageSize = new Size(40, 40);
     
     return w;
 }
@@ -105,10 +110,21 @@ async function loadImage(url) {
     return await req.loadImage(); 
 }
 
-async function fetchParagraphData() {
-   // Mock API call to get a paragraph (replace with actual API call)
-   return "This is a sample paragraph fetched from an API or RSS feed.";
+async function todoApi() {
+    let docs = await loadDocs()
+    return {
+        name: docs["todo"],
+        description: docs["context"],
+        url: docs["url"]
+    }
 }
+
+async function loadDocs() {
+    let url = "https://static-logosm.s3.ap-south-1.amazonaws.com/cal.json" // change this to your calender endpoint which fetches latest activity to do
+    let req = new Request(url)
+    return await req.loadJSON()
+  }
+  
 
 async function checkWebsites(websites) {
    let statuses = [];
